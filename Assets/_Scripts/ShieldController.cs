db77560eb162af768443c43ef6386e32f5542b21
B00059819 David Cushen <cushen.dave@gmail.com>
2018-01-12 16:05:01 +0000

    upload of test

---
+++ Assets/_Scripts/ShieldController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour
+{
+	public ShieldModel sm;
+	public ShieldView sView;
+
+	// Use this for initialization
+	void Start () {
+		sm = new ShieldModel ();
+		sView = GetComponent<ShieldView> ();
+
+
+	}
+
+	void takeDamage(int damage) { 
+		sm.TakeDamage (damage);
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/ShieldView.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldView : MonoBehaviour {
+	private Button oneDamage, tenDamage, tickDamage;
+	private ShieldModel shieldModel;
+	public ShieldController sController;
+
+	void Awake() { 
+	}
+	// Use this for initialization
+	void Start () {
+		oneDamage.onClick.AddListener (sController.takeDamage(1));
+		tenDamage.onClick.AddListener (sController.takeDamage(10));
+		tickDamage.onClick.AddListener (sController.tick());
+	}
+
+	void takeDamage() {
+		oneDamage.onClick (shieldModel.TakeDamage (1));
+	}
+
+	public void SetModel() { 
+		this.shieldModel = shieldModel;
+	}
+
+	public void ChangeHealth() { 
+		if (shieldModel.GetStrength () > 90) { 
+			
+		}
+
+	}
+
+	public void SetUIText(Text isRepairable, Text Strength) { 
+		if (shieldModel.IsRepairable.equals (true)) { 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
